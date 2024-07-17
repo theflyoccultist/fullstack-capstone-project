cdb48fa1bb2aff147bfe7ec6bfb89b37385db2fc
theflyoccultist <y.delahaije@gmail.com>
2024-07-17 13:28:48 -0400

    user profile back end

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -6,6 +6,8 @@ const router = express.Router();
 const dotenv = require('dotenv');
 const pino = require('pino');
 
+const { body, validationResult } = require('express-validator');
+
 const logger = pino();
 
 dotenv.config();
@@ -82,6 +84,50 @@ router.post('/login', async (req, res) => {
     } catch (e) {
          return res.status(500).send('Internal server error');
     }
+});
+
+router.put('/update', async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Validation errors in update request', errors.array());
+        return res.status(400).json({ errors: errors.array() });
+    }
+try {
+    const email = req.headers.email;
+    if (!email) {
+        logger.error('Email not found in the request headers');
+        return res.status(400).json({error: "Email not found in the request headers"});
+    }
+    const db = await connectToDatabase();
+    const collection = db.collection("users");
+    const existingUser = await collection.findOne({ email });
+
+    if (!existingUser) {
+        logger.error('User not found');
+        return res.status(404).json({ error: "User not found" });
+    }
+
+    existingUser.firstName = req.body.name;
+    existingUser.updatedAt = new Date();
+
+    const updatedUser = await collection.findOneAndUpdate(
+        { email },
+        { $set: existingUser },
+        { returnDocument: 'after' }
+    );
+    const payload = {
+        user: {
+            id: updatedUser._id.toString(),
+        },
+    };
+    const authtoken = jwt.sign(payload, JWT_SECRET);
+    logger.info('User updated successfully');
+
+    res.json({authtoken});
+} catch (e) {
+    logger.error(e);
+     return res.status(500).send('Internal server error');
+}
 });
 
 module.exports = router;
