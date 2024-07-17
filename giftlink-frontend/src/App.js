1d101bdb91fb404599133e177efc0359c1c901a9
theflyoccultist <y.delahaije@gmail.com>
2024-07-17 13:45:53 -0400

    profile implement

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -8,6 +8,7 @@ import SearchPage from './components/SearchPage/SearchPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
 
 function App() {
     const navigate = useNavigate();
@@ -21,6 +22,7 @@ function App() {
             <Route path="/app/register" element={<RegisterPage/>} />
             <Route path="/app/product/:productId" element={<DetailsPage/>} />
             <Route path="/app/search" element={<SearchPage/>} />
+            <Route path="/app/profile" element={<Profile/>} />
         </Routes>
         </>
   );
