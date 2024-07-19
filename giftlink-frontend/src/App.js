1e36e155258502af0023bcb0a30bbfc1adc03f78
Rin <131471854+theflyoccultist@users.noreply.github.com>
2024-07-19 21:18:07 +0200

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,6 @@
+/*jshint esversion: 8 */
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
